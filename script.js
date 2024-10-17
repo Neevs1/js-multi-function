158f9541c8078f35c4d87456eebe5941d558fad0
Neevs1 <neevan.redkar@gmail.com>
2024-10-17 19:55:57 +0530

    Made modulus more primitive, changes to input function

--- script.js
+++ script.js
@@ -35,14 +35,13 @@ function clearResult(){
 }
 
 function input(){
-    return document.getElementById("inp").value;
+    document.getElementById("result").innerHTML ="Inputted value is"+ document.getElementById("inp").value;
 }
 function mod(){
-    document.getElementById("result").innerHTML = "Enter first number and hit enter";
-    let num1 = parseInt(document.getElementById("enter").addEventListener("click", input()));
-    document.getElementById("result").innerHTML = "Enter second number and hit enter";
-    let num2 = parseInt(document.getElementById("enter").addEventListener("click", input()));
-    document.getElementById("result").innerHTML = "Remainder is " + num1 % num2;
+    let num1 = parseInt(prompt("Enter the second number: "));
+    let num2 = parseInt(prompt("Enter the second number: "));
+    num3 = num1 % num2;
+    document.getElementById("result").innerHTML = "The modulus of the two numbers is: " + num3;
     
 
 }
