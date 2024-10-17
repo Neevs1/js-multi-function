a69337e6ad2c6d07ebf85ce23362d4e635f3331b
Neevs1 <neevan.redkar@gmail.com>
2024-10-17 19:09:31 +0530

    Added bubble sort

--- script.js
+++ script.js
@@ -25,3 +25,33 @@ function divider(){
     num3 = num1 / num2; 
     document.getElementById("result").innerHTML = "The division of the two numbers is: " + num3;
 }
+
+function clearResult(){
+    document.getElementById("result").innerHTML = "";
+}
+
+function bubbleSort(){
+   let length =  prompt("Enter length of array: ");
+   let arr = [];
+   for (let i = 0; i < length; i++){
+       arr.push(parseInt(prompt("Enter element " + (i + 1) + ": ")));
+   }
+    let n = arr.length;
+    let steps = "<ol>";
+    for(let i=0;i<n;i++){
+        for(let j=0;i<n;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                steps+= "<li>"
+                for(let k=0;k<n;k++){
+                    steps+= arr[k] + " ";
+                }
+                steps=+ "</li>";
+            }
+        }
+    }
+    steps+= "</ol>";
+    document.getElementById("result").innerHTML = steps;
+}
